3f27a9c89b4fa8810b3be30f3ffea6e5a859aef3
Aleksej Tapekhin <TapAleksej@yandex.ru>
2025-07-18 11:05:31 +0000

    my rem

--- 14BashShell3/scripts/bad_ipssh.sh
+++ 14BashShell3/scripts/bad_ipssh.sh
@@ -6,7 +6,6 @@ filename="blocklist.txt"
 
 active_ssh_ip=$(who | awk '{print $5}' | sed 's/[()]//g')
 
-
 for ip in $(cat "$filename");do
     if echo "$active_ssh_ip" | grep -q "^$ip$"; then
       echo "${ip} попался"
