313ae2565afff25c27d57d77443a0b73fd0434c9
Aleksej Tapekhin <TapAleksej@yandex.ru>
2025-07-18 07:57:43 +0000

    autocommit_5ae29a0

--- auto_commit.sh
+++ auto_commit.sh
@@ -2,20 +2,27 @@
 set -o pipefail
 set -x
 
-git add .		
-		
+git add .
+
 last_hash=$(git log --oneline | head -1 | awk '{print $1}')
 
 
-git commit -m "autocommit_${last_hash}" 
+git commit -m "autocommit_${last_hash}"
 
 echo "return code  $?"
 chk=$(git log --oneline | head -2)
- 
+
 echo "$chk"
 
-git pull
-if [ $? -eq 0 ]; then
-  git push
+echo "Сделать отправку на гитхаб ? y/n"
+
+read answer
+
+
+if [ ${answer} -eq 'y' ]; then
+	git pull 
+
+	if [ $? -eq 0 ]; then
+	  git push
+	fi
 fi
-#echo "autocommit_$last_hash"
